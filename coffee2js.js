4787bd32d5d280121425c4d73528560cf1b786e2
souparno <souparno.majumder@gmail.com>
2018-12-20 22:28:27 +0530

    code cleanup and running the js file directly without requiring it

--- coffee2js.js
+++ coffee2js.js
@@ -1,187 +1,173 @@
-var sh = require("shelljs");
-var mkdirp = require('mkdirp');
-var debug=require('debug')('coffee2jskeepcomments')
-	,fs=require('fs')
-	//,carrier = require('carrier')
-	,cs=require('coffee-script')
-	,os=require('os')
-;
+var sh = require("shelljs"),
+        mkdirp = require('mkdirp'),
+        debug = require('debug')('coffee2jskeepcomments'),
+        fs = require('fs'),
+        cs = require('coffee-script'),
+        path = require('path'),
+        lineReader = require('line-reader');
 
-var path = require('path');  
-var lineReader = require('line-reader');
-
-/*
-process.stdin.resume();
-carrier.carry(process.stdin, function(line) {
-    console.log('got one line: ' + line);
-});
-*/
-
-function help(){
-	console.log('Usage: xxx file1.coffee file2.coffee');
-        console.log('Usage: xxx -c compile-dir -o output-dir');
+function help() {
+    console.log('Usage: xxx file1.coffee file2.coffee');
+    console.log('Usage: xxx -c compile-dir -o output-dir');
 }
 
-function compilefile(p, outfile, cb){
+function compilefile(p, outfile, cb) {
 
-	var inblockcmts=false;
-	
-	outfile = outfile || path.dirname(p)+path.sep+path.basename(p,path.extname(p))+ '.js';
-	//console.log('outfile='+ outfile);
-	//var fout=fs.createWriteStream(p);
-	var src='';
-	
-	var linenum=0;
-	
-	lineReader.eachLine(p, function(line, last) {
-		linenum ++;
-		//debug(linenum+"\t"+line);
+    var inblockcmts = false;
 
-		//block comment always start from 0?
-		var i=line.indexOf('###');
-		if (i>=0){
-			inblockcmts = !inblockcmts;
-			debug('in comments, linenum='+linenum+',inblockcmts='+inblockcmts);
-			if (i===0){
-				i=line.indexOf('###', 3);	//oneline block comments
-				if (i>0){
-					inblockcmts = !inblockcmts;
-					debug('oneline block comments, linenum='+linenum+',inblockcmts='+inblockcmts);
-				}
-			}
-		} else if (!inblockcmts && line.indexOf('#') ===0 ){
-			debug('singleline comments, linenum='+linenum+',inblockcmts='+inblockcmts);
-			//oneline comments
-			line = line.replace('#', '');
-			line = '###' + line + '###';
-		}
-		src += line + "\n";
-				
-		if(last){
-			// or check if it's the last one
-			//for debugging purpose
-			//fs.writeFile(p+'.tmp.coffee', src);
-			
-			var dst='';
-			try {
-				dst=cs.compile(src, {bare: true});
-			} catch (e){
-				console.log(e);
-			}
+    outfile = outfile || path.dirname(p) + path.sep + path.basename(p, path.extname(p)) + '.js';
+    var src = '';
 
-			//debug('dst=' + dst);
-			//fs.writeFile(outfile+".tmp2.js", dst);			
-			
-			//Now, one problem is that there is two more empty lines after each /** **/
-			//var lines=dst.split('\r');	//Not working?
-			//var lines = dst.match(/^.*([\n\r]+|$)/gm);
-			var lines = dst.split(/\r\n|\r|\n/g);
-			
-			debug("lines.length="+lines.length);
-			var dst2='';
-			for (var i = 0; i < lines.length; i++){
-				line=lines[i];
-				var x=line.indexOf('/*');
-				var y=line.indexOf('*/');
-				if (x>=0 && y>0){
-					debug('single line comments at:'+i);
-					line = line.replace('/*', '//');
-					debug('****line='+line);
-					line =line.replace('*/', '');
-				}
-				dst2 += line + "\n";
-			}
+    var linenum = 0;
 
-			fs.writeFile(outfile, dst2, function(err){
-                          if(err) console.log(err);
+    lineReader.eachLine(p, function (line, last) {
+        linenum++;
+        //debug(linenum+"\t"+line);
 
-                          if(cb) cb();
-                        });
-		}
-	});
+        //block comment always start from 0?
+        var i = line.indexOf('###');
+        if (i >= 0) {
+            inblockcmts = !inblockcmts;
+            debug('in comments, linenum=' + linenum + ',inblockcmts=' + inblockcmts);
+            if (i === 0) {
+                i = line.indexOf('###', 3);	//oneline block comments
+                if (i > 0) {
+                    inblockcmts = !inblockcmts;
+                    debug('oneline block comments, linenum=' + linenum + ',inblockcmts=' + inblockcmts);
+                }
+            }
+        } else if (!inblockcmts && line.indexOf('#') === 0) {
+            debug('singleline comments, linenum=' + linenum + ',inblockcmts=' + inblockcmts);
+            //oneline comments
+            line = line.replace('#', '');
+            line = '###' + line + '###';
+        }
+        src += line + "\n";
+
+        if (last) {
+            // or check if it's the last one
+            //for debugging purpose
+            //fs.writeFile(p+'.tmp.coffee', src);
+
+            var dst = '';
+            try {
+                dst = cs.compile(src, {bare: true});
+            } catch (e) {
+                console.log(e);
+            }
+
+            //debug('dst=' + dst);
+            //fs.writeFile(outfile+".tmp2.js", dst);			
+
+            //Now, one problem is that there is two more empty lines after each /** **/
+            //var lines=dst.split('\r');	//Not working?
+            //var lines = dst.match(/^.*([\n\r]+|$)/gm);
+            var lines = dst.split(/\r\n|\r|\n/g);
+
+            debug("lines.length=" + lines.length);
+            var dst2 = '';
+            for (var i = 0; i < lines.length; i++) {
+                line = lines[i];
+                var x = line.indexOf('/*');
+                var y = line.indexOf('*/');
+                if (x >= 0 && y > 0) {
+                    debug('single line comments at:' + i);
+                    line = line.replace('/*', '//');
+                    debug('****line=' + line);
+                    line = line.replace('*/', '');
+                }
+                dst2 += line + "\n";
+            }
+
+            fs.writeFile(outfile, dst2, function (err) {
+                if (err)
+                    console.log(err);
+
+                if (cb)
+                    cb();
+            });
+        }
+    });
 }
 
-var walk = function(dir, done) {
-        var results = [];
-        fs.readdir(dir, function(err, list) {
-            if (err) return done(err);
-            var i = 0;
-            (function next() {
-                var file = list[i++];
-                if (!file) return done(null, results);
-                file = dir + '/' + file;
-                fs.stat(file, function(err, stat) {
-                    if (stat && stat.isDirectory()) {
-                        walk(file, function(err, res) {
-                            results = results.concat(res);
-                            next();
-                        });
-                    } else {
-                        results.push(file);
+var walk = function (dir, done) {
+    var results = [];
+    fs.readdir(dir, function (err, list) {
+        if (err)
+            return done(err);
+        var i = 0;
+        (function next() {
+            var file = list[i++];
+            if (!file)
+                return done(null, results);
+            file = dir + '/' + file;
+            fs.stat(file, function (err, stat) {
+                if (stat && stat.isDirectory()) {
+                    walk(file, function (err, res) {
+                        results = results.concat(res);
                         next();
-                    }
-                });
-            })();
-        });
+                    });
+                } else {
+                    results.push(file);
+                    next();
+                }
+            });
+        })();
+    });
+};
+
+(function () {
+    var args = process.argv.slice(2);
+    if (!args.length) {
+        help();
+        return;
     }
 
-function main(args){
-        var args = args || process.argv.slice(2);
-	if (!args.length){
-		help();
-		return;
-	}
-
-
-	if(args[0] == "-c") {
-           if(args[2] == "-o") {
-             var cwd = sh.pwd();
-             var compile_dir = args[1];
-             var output_dir = cwd.stdout + "/" + args[3];
+    if (args[0] == "-c") {
+        if (args[2] == "-o") {
+            var cwd = sh.pwd();
+            var compile_dir = args[1];
+            var output_dir = cwd.stdout + "/" + args[3];
             //console.log(output_dir);
             //fs.emptydir(output_dir); 
-             walk(compile_dir, function (err, results) {
-               if(err) throw err;
+            walk(compile_dir, function (err, results) {
+                if (err)
+                    throw err;
 
 
-         var index = 0;
-         
-        (function rec() {
-                 var val = results[index++];
-                 if(index > results.length) return;
-                 var re = /(?:\.([^.]+))?$/;
-                 var ext = re.exec(val)[1];
-                 var dir = output_dir + val.substr(compile_dir.length);
-                 var outputfile = dir.substr(dir.lastIndexOf("/"));
-                 dir = dir.substr(0, dir.lastIndexOf("/"));
-                 output_file = dir + outputfile.substr(0, outputfile.lastIndexOf(".")) + ".js"; 
+                var index = 0;
 
-                 mkdirp.sync(dir);
-                 if(ext == "coffee"){
-                   console.log(output_file + "  -- done");
-                   compilefile(val, output_file, rec);
-                 } else {
-                   console.log(val + " copied from src dir");
-                   var src = fs.readFileSync(val).toString();
-                   fs.writeFileSync(output_file, src); 
-                   rec();
-                 }
-         }());
-             }); 
-             return;
-           }
-           help();
-           return;
+                (function rec() {
+                    var val = results[index++];
+                    if (index > results.length)
+                        return;
+                    var re = /(?:\.([^.]+))?$/;
+                    var ext = re.exec(val)[1];
+                    var dir = output_dir + val.substr(compile_dir.length);
+                    var outputfile = dir.substr(dir.lastIndexOf("/"));
+                    dir = dir.substr(0, dir.lastIndexOf("/"));
+                    output_file = dir + outputfile.substr(0, outputfile.lastIndexOf(".")) + ".js";
+
+                    mkdirp.sync(dir);
+                    if (ext == "coffee") {
+                        console.log(output_file + "  -- done");
+                        compilefile(val, output_file, rec);
+                    } else {
+                        console.log(val + " copied from src dir");
+                        var src = fs.readFileSync(val).toString();
+                        fs.writeFileSync(output_file, src);
+                        rec();
+                    }
+                }());
+            });
+            return;
         }
-	
-	args.forEach(function (val, index, array) {
-		console.log('Compiling:'+ index + ': ' + val);
-		compilefile(val);
-	});
-}
+        help();
+        return;
+    }
 
-if (typeof module !== 'undefined' && module.exports) {
-  module.exports = main;
-} else {
-  main();
-}
+    args.forEach(function (val, index, array) {
+        console.log('Compiling:' + index + ': ' + val);
+        compilefile(val);
+    });
+}());
